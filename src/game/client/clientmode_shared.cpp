5896be9cafc50f1740d97408023d71eec2de59d2
Bitl <bitlblox@gmail.com>
2026-06-06 10:27:41 -0700

    aesthetic changes

--- src/game/client/clientmode_shared.cpp
+++ src/game/client/clientmode_shared.cpp
@@ -41,6 +41,10 @@
 #include "xbox/xbox_console.h"
 #endif
 
+// Fenix: Needed for the custom background loading screens
+#include "GameUI/IGameUI.h"
+#include "mapload_background.h"
+
 #if defined( REPLAY_ENABLED )
 #include "replay/replaycamera.h"
 #include "replay/ireplaysystem.h"
@@ -78,6 +82,11 @@ class CHudVote;
 
 static vgui::HContext s_hVGuiContext = DEFAULT_VGUI_CONTEXT;
 
+// Fenix: Needed for the custom background loading screens
+// See interface.h/.cpp for specifics: basically this ensures that we actually Sys_UnloadModule the dll and that we don't call Sys_LoadModule 
+// over and over again.
+static CDllDemandLoader g_GameUI("GameUI");
+
 ConVar cl_drawhud( "cl_drawhud", "1", FCVAR_CHEAT, "Enable the rendering of the hud" );
 ConVar hud_takesshots( "hud_takesshots", "0", FCVAR_CLIENTDLL | FCVAR_ARCHIVE, "Auto-save a scoreboard screenshot at the end of a map." );
 ConVar hud_freezecamhide( "hud_freezecamhide", "0", FCVAR_CLIENTDLL | FCVAR_ARCHIVE, "Hide the HUD during freeze-cam" );
@@ -88,6 +97,9 @@ extern ConVar voice_modenable;
 extern ConVar cl_enable_text_chat;
 
 extern bool IsInCommentaryMode( void );
+// Fenix: Needed for the custom background loading screens
+CMapLoadBG* pPanelBg;
+IMaterial* pMatMapBg;
 extern const char* GetWearLocalizationString( float flWear );
 
 CON_COMMAND( cl_reload_localization_files, "Reloads all localization files" )
@@ -293,6 +305,10 @@ ClientModeShared::ClientModeShared()
 	m_pWeaponSelection = NULL;
 	m_nRootSize[ 0 ] = m_nRootSize[ 1 ] = -1;
 
+	// Fenix: Needed for the custom background loading screens
+	pPanelBg = NULL;
+	pMatMapBg = NULL;
+
 #if defined( REPLAY_ENABLED )
 	m_pReplayReminderPanel = NULL;
 	m_flReplayStartRecordTime = 0.0f;
@@ -389,6 +405,22 @@ void ClientModeShared::Init()
 
 	HOOK_MESSAGE( VGUIMenu );
 	HOOK_MESSAGE( Rumble );
+
+	// Fenix: Custom background loading screens - Injects the custom panel at the loading screen
+	CreateInterfaceFn gameUIFactory = g_GameUI.GetFactory();
+	if (gameUIFactory)
+	{
+		IGameUI* pGameUI = (IGameUI*)gameUIFactory(GAMEUI_INTERFACE_VERSION, NULL);
+		if (pGameUI)
+		{
+			// Insert custom loading panel for the loading dialog
+			pPanelBg = new CMapLoadBG("Background");
+			pPanelBg->InvalidateLayout(false, true);
+			pPanelBg->SetVisible(false);
+			pPanelBg->MakePopup(false);
+			pGameUI->SetLoadingBackgroundDialog(pPanelBg->GetVPanel());
+		}
+	}
 }
 
 
@@ -897,6 +929,27 @@ void ClientModeShared::LevelInit( const char *newmap )
 	// Reset any player explosion/shock effects
 	CLocalPlayerFilter filter;
 	enginesound->SetPlayerDSP( filter, 0, true );
+
+	// Fenix: Custom background loading screens - decides if use a loading screen for a map or a default one
+#ifdef _WIN32
+	char szMapBgName[MAX_PATH];
+#else	// !_WIN32
+	char szMapBgName[PATH_MAX];
+#endif	// _WIN32
+
+	Q_snprintf(szMapBgName, sizeof(szMapBgName), "vgui/loading/maps/%s", newmap);
+
+	pMatMapBg = materials->FindMaterial(szMapBgName, TEXTURE_GROUP_OTHER);
+
+	if (!pMatMapBg->IsErrorMaterial())
+	{
+		Q_snprintf(szMapBgName, sizeof(szMapBgName), "loading/maps/%s", newmap);
+		pPanelBg->SetNewBackgroundImage(szMapBgName);
+	}
+	else
+	{
+		pPanelBg->SetNewBackgroundImage("loading/default");
+	}
 }
 
 //-----------------------------------------------------------------------------
