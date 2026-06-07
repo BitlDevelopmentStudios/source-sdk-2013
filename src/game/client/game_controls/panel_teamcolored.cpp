dfbadf2243d81d0b1455e3a500190cb6babf2338
Bitl <bitlblox@gmail.com>
2026-06-06 18:31:26 -0700

    add CRichTextTeamColored

--- src/game/client/game_controls/panel_teamcolored.cpp
+++ src/game/client/game_controls/panel_teamcolored.cpp
@@ -12,3 +12,4 @@ DECLARE_BUILD_FACTORY(CEditablePanelTeamColored);
 DECLARE_BUILD_FACTORY(CFrameTeamColored);
 DECLARE_BUILD_FACTORY_DEFAULT_TEXT(CLabelTeamColored, CLabelTeamColored);
 DECLARE_BUILD_FACTORY_DEFAULT_TEXT(CButtonTeamColored, CButtonTeamColored);
+DECLARE_BUILD_FACTORY(CRichTextTeamColored);
