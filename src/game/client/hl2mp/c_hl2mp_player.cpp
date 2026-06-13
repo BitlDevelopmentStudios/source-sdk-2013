8f3ba70ae862faad88cff345b1440b41c8136aa4
Bitl <bitlblox@gmail.com>
2026-06-13 15:10:04 -0700

    fix blood effects in friendly fire mode

--- src/game/client/hl2mp/c_hl2mp_player.cpp
+++ src/game/client/hl2mp/c_hl2mp_player.cpp
@@ -188,8 +188,11 @@ void C_HL2MP_Player::TraceAttack( const CTakeDamageInfo &info, const Vector &vec
 
 		if ( pAttacker )
 		{
-			if ( HL2MPRules()->IsTeamplay() && pAttacker->InSameTeam( this ) == true )
-				return;
+			if (pAttacker->InSameTeam(this) == true)
+			{
+				if (!friendlyfire.GetInt() && HL2MPRules()->PlayerRelationship(this, info.GetAttacker()) == GR_TEAMMATE)
+					return;
+			}
 		}
 
 		if ( blood != DONT_BLEED )
