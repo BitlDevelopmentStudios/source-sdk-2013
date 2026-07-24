6ae90110be96f795800df015aba541c0cd262071
Bitl <bitlblox@gmail.com>
2026-07-23 21:39:40 -0700

    implement cvar tags for serverfinder

--- src/game/shared/hl2mp/hl2mp_gamerules.h
+++ src/game/shared/hl2mp/hl2mp_gamerules.h
@@ -104,6 +104,7 @@ public:
 	virtual float FlWeaponTryRespawn( CBaseCombatWeapon *pWeapon );
 	virtual Vector VecWeaponRespawnSpot( CBaseCombatWeapon *pWeapon );
 	virtual int WeaponShouldRespawn( CBaseCombatWeapon *pWeapon );
+	virtual void GetTaggedConVarList(KeyValues* pCvarTagList);
 	virtual void Think( void );
 	virtual void CreateStandardEntities( void );
 	virtual void ClientSettingsChanged( CBasePlayer *pPlayer );
