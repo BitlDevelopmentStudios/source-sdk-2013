c122c66c6b74cd2cfe7703ec8d172610dff1718f
Bitl <bitlblox@gmail.com>
2026-08-30 15:58:39 -0700

    a

--- src/game/shared/hl2mp/hl2mp_gamerules.cpp
+++ src/game/shared/hl2mp/hl2mp_gamerules.cpp
@@ -1579,7 +1579,7 @@ void CHL2MPRules::DeathNotice( CBasePlayer *pVictim, const CTakeDamageInfo &info
 	CBaseEntity *pKiller = info.GetAttacker();
 	CBasePlayer *pScorer = GetDeathScorer( pKiller, pInflictor );
 
-	if (sv_friendlyfire_deathnotice.GetBool() && (pScorer->GetTeamNumber() == pVictim->GetTeamNumber()))
+	if (!sv_friendlyfire_deathnotice.GetBool() && (pScorer->GetTeamNumber() == pVictim->GetTeamNumber()))
 		return;
 
 	// Custom kill type?
