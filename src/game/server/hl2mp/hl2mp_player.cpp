594fcf1a394e148908a829bd6c4d00e5cc564f5f
Bitl <bitlblox@gmail.com>
2026-09-14 17:47:34 -0700

    Squashed commit of the following:
    commit ec354494b5c2224614733028490473c8bff45d07
    Author: Brandon Little <blittle@inco.cc>
    Date:   Tue Sep 1 11:39:21 2026 -0400
    
        Fix stunstick activation sounds

--- src/game/server/hl2mp/hl2mp_player.cpp
+++ src/game/server/hl2mp/hl2mp_player.cpp
@@ -245,6 +245,8 @@ void CHL2MP_Player::GiveAllItems( void )
 	CBasePlayer::GiveAmmo( 1,	"grenade" );
 	CBasePlayer::GiveAmmo( 2,	"slam" );
 
+	GiveNamedItem( "weapon_physcannon" );
+
 	GiveNamedItem( "weapon_crowbar" );
 	GiveNamedItem( "weapon_stunstick" );
 	GiveNamedItem( "weapon_pistol" );
@@ -261,8 +263,6 @@ void CHL2MP_Player::GiveAllItems( void )
 	GiveNamedItem( "weapon_rpg" );
 
 	GiveNamedItem( "weapon_slam" );
-
-	GiveNamedItem( "weapon_physcannon" );
 	
 }
 
@@ -276,6 +276,8 @@ void CHL2MP_Player::GiveDefaultItems( void )
 	CBasePlayer::GiveAmmo( 6,	"Buckshot");
 	CBasePlayer::GiveAmmo( 6,	"357" );
 
+	GiveNamedItem( "weapon_physcannon" );
+
 	if ( GetPlayerModelType() == PLAYER_SOUNDS_METROPOLICE || GetPlayerModelType() == PLAYER_SOUNDS_COMBINESOLDIER )
 	{
 		GiveNamedItem( "weapon_stunstick" );
@@ -288,7 +290,6 @@ void CHL2MP_Player::GiveDefaultItems( void )
 	GiveNamedItem( "weapon_pistol" );
 	GiveNamedItem( "weapon_smg1" );
 	GiveNamedItem( "weapon_frag" );
-	GiveNamedItem( "weapon_physcannon" );
 
 	const char *szDefaultWeaponName = engine->GetClientConVarValue( engine->IndexOfEdict( edict() ), "cl_defaultweapon" );
 
